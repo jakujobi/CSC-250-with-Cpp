b1281796796014ce72eabbc6e6beebe285f54146
akujobi365 <akujobi365@gmail.com>
2023-02-22 22:39:35 -0600

    Update date_cstring_jca.cpp
    Added new functions

--- Homeworks and Projects/Program 4 - Print Date Words from Date/C-style Strings/date_cstring_jca.cpp
+++ Homeworks and Projects/Program 4 - Print Date Words from Date/C-style Strings/date_cstring_jca.cpp
@@ -1,18 +1,123 @@
-#include <iostream>
-#include <cstring>
+#include <iostream> // for cout, cin, endl
+#include <cstring> // for strlen, strcpy, strcmp
 
 using namespace std;
 
-
-
+//THE ARRAY OF STRINGS FOR THE WORDS
+// The array of strings for the ones place
 const char* const ones[] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
 //char* ones[] = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
+// The array of strings for the tens place
 const char* const tens[] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
 //char* tens[] = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
 
+// The array of strings for the teens
 const char* const elevenToNineteen[] = {"Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 //char* elevenToNineteen[] = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+
+
+// THE FUNCTIONS
+
+// This function will get the input from the user
+void getInput(char* date);
+void getInput(char* date)
+{
+    // Ask the user to enter a date in the format MM/DD/YYYY
+    cout << "Enter a date in the format MM/DD/YYYY: ";
+    cin.getline(date, 11);
+}
+
+
+// Prints the error message which will
+//Then be followed by the reason why the date is invalid contained in the function checkDateFormat
+void printError ();
+void printError ()
+{
+    cout << "Error\n  with dateThe date is invalid\nPlease make sure that the date is in the format MM/DD/YYYY\n";
+}
+
+
+int checkDateFormat(char* date)
+{
+    // Check if the input string has length 10
+    if (strlen(date) != 10)
+    {
+        printError(); //Prints the error message
+        cout << "It seems that there are more characters than needed\n";
+        return 1;
+    }
+
+    // Check if the first two characters are digits representing a valid month
+    int month = stoi(string(date).substr(0, 2));
+    if (month < 1 || month > 12)
+    {
+        printError();   //Prints the error message
+        cout << "The month must be between 1 and 12\n";
+        return 1;
+    }
+
+    // Check if the third character is a forward slash
+    if (date[2] != '/')
+    {
+        printError();   //Prints the error message
+        cout << "The third character is not a forward slash\n";
+        return 1;
+    }
+
+    // Check if the fourth and fifth characters are digits representing a valid day
+    int day = stoi(string(date).substr(3, 2));
+    if (day < 1 || day > 31)
+    {
+        printError();   //Prints the error message
+        cout << "The day must be between 1 and 31\n";
+        return 1;
+    }
+
+    // Check if the sixth character is a forward slash
+    if (date[5] != '/')
+    {
+        printError(); //Prints the error message
+        cout << "The sixth character is not a forward slash\n";
+        return 1;
+    }
+
+    // Check if the last four characters are digits representing a valid year
+    int year = stoi(string(date).substr(6, 4));
+    if (year < 0 || year > 9999)
+    {
+        printError(); //Prints the error message
+        cout << "The year must be between 0 and 9999\n";
+        return 1;
+    }
+
+    //check for invalid days of february
+    // Just in case
+    // Though it might not work for 29 on non-leap years
+    if (month == 2 && day > 29)
+    {
+        cout << "\nError!\nFebruary only has 29 days in a leap year\n";
+        return 1;
+    }
+
+    //check for invalid days of months with 30 days
+    //Like April, June, September, and November
+    if (month == 4 || month == 6 || month == 9 || month == 11)
+    {
+        if (day > 30)
+        {
+            cout << "\nError!\nThis month only has 30 days\n";
+            return 1;
+        }
+    }
+
+    // If all checks pass, then return true
+    //Woohoo!
+    cout << "Good Job, The date is valid\n";
+    return 0;
+}
+
+
 
 
 // This function prints a given number in words
@@ -127,6 +232,88 @@ void convertToWords(char* num)
     }
 }
 
+void convertToWords(char* date);
+void convertToWords(char* date)
+{
+    // Get the number of digits in the given number using the length of the string
+    //then using strtok to get the month, day, and year
+    //then using stoi to convert the month, day, and year to ints
+    //strtok function description: https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
+    int month = stoi(strtok(date, "/"));    // Get the month
+    int day = stoi(strtok(NULL, "/"));
+    int year = stoi(strtok(NULL, "/"));
+
+    if (month < 1 || month > 12)
+    {
+        cout << "Invalid month" << endl;
+        return;
+    }
+    if (day < 1 || day > 31)
+    {
+        cout << "Invalid day" << endl;
+        return;
+    }
+    if (year < 0 || year > 9999)
+    {
+        cout << "Invalid year" << endl;
+        return;
+    }
+
+    cout << months[month] << " " << ones[day] << " ";
+
+    if (day >= 11 && day <= 19)
+    {
+        cout << "th";
+    }
+    else if (day % 10 == 1)
+    {
+        cout << "st";
+    }
+    else if (day % 10 == 2)
+    {
+        cout << "nd";
+    }
+    else if (day % 10 == 3)
+    {
+        cout << "rd";
+    }
+    else
+    {
+        cout << "th";
+    }
+
+    cout << ", ";
+
+    if (year >= 1000)
+    {
+        cout << ones[year / 1000] << " thousand ";
+        year %= 1000;
+    }
+    if (year >= 100)
+    {
+        cout << ones[year / 100] << " hundred ";
+        year %= 100;
+    }
+    if (year >= 20)
+    {
+        cout << tens[year / 10] << " ";
+        year %= 10;
+    }
+    if (year >= 11 && year <= 19)
+    {
+        cout << elevenToNineteen[year - 11];
+    }
+    else if (year >= 1 && year <= 9)
+    {
+        cout << ones[year];
+    }
+    else if (year == 10)
+    {
+        cout << "ten";
+    }
+
+    cout << endl;
+}
 
 
 
@@ -137,7 +324,44 @@ int main()
 {
     // Declare variables
     int num;    // Integer to be converted to words
+    char date[11]; // C-string to store the date
+
+    // Convert date to words
+    convertToWords(date);
+    
     char numString[100];    // C-string to store the integer
+
+    // Print the program welcome message
+    cout << "Welcome to the Number to Words Converter!\n";
+
+    //A loop that asks the user for input and checks if the input is valid using the checkDate function
+    //If the input is valid, the loop breaks
+    //If the input is invalid, the loop continues
+    while (true)
+    {
+        // Get user input
+        getInput(date);
+
+
+        // Convert integer to C-string
+        sprintf(numString, "%d", num);
+
+        // Call the function to convert to words
+        convertToWords(numString);
+
+        // Ask the user if they want to continue
+        cout << "Do you want to continue? (y/n): ";
+        char choice;
+        cin >> choice;
+
+        // If the user does not want to continue, break the loop
+        if (choice == 'n' || choice == 'N')
+        {
+            break;
+        }
+    }
+
+
 
     // Ask our user for input
     cout << "Enter an integer: ";
