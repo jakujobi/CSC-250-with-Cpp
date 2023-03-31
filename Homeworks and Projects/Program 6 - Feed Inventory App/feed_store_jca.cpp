c553732921fde7174bf8e5bb9d21ee4fc2a6d2bd
akujobi365 <akujobi365@gmail.com>
2023-03-31 15:03:43 -0500

    Update feed_store_jca.cpp

--- Homeworks and Projects/Program 6 - Feed Inventory App/feed_store_jca.cpp
+++ Homeworks and Projects/Program 6 - Feed Inventory App/feed_store_jca.cpp
@@ -70,6 +70,8 @@ void Line(int num) {
         cout << "-";
     }
     cout << endl;
+
+    //cout << setfill('-') << setw(50) << "-" << setfill(' ') << endl;
 }
 
 
@@ -133,19 +135,19 @@ void addItem() {
     cout << endl;
     Line(16);
     cout << "ADD ITEM" << endl;
-    Line(16);
+    //Line(16);
 
 
     cout << "\nWith " << MAX_CHAR << " maximum characters,\n"
         << "Enter the item name -->: ";
     cin.getline(newItem.item, MAX_CHAR); //get the item name from the user
-    cin.ignore();
-    cin.clear();
+    // cin.ignore();
+    // cin.clear();
     cout << newItem.item << " has been added to the inventory." << endl;
 
     //ask the user for the item quantity
     do {
-        cout << "Enter the quantity of bags -->: ";
+        cout << "\nEnter the quantity of bags -->: ";
         cin >> newItem.qty; //get the item quantity from the user
         cin.ignore();
         cin.clear();
@@ -157,7 +159,7 @@ void addItem() {
 
     //ask the user for the item price
     do {
-        cout << "Enter the price per bag of the item-->: ";
+        cout << "\nEnter the price per bag of the item-->: ";
         cin >> newItem.price; //get the item price from the user
         cin.ignore();
         cin.clear();
@@ -178,11 +180,11 @@ void addItem() {
     outFile.write(reinterpret_cast<const char*>(&newItem), sizeof(newItem));
     outFile.close();
 
-    cout << "Item added successfully!" << endl;
+    cout << "\nItem added successfully!" << endl;
 
     //ask the user if they want to add another item
     //uses recursive by calling addItem() again
-    cout << "Do you want to add another item? (y/n): ";
+    cout << "\nDo you want to add another item? (y/n): ";
     char repeat;
     cin >> repeat;
     cin.ignore();
@@ -196,21 +198,139 @@ void addItem() {
 }
 
 
+// Function to print the inventory in table form
 void printInventory() {
+
+    //Print Header for function
+    cout << endl;
+    Line(50);
+    cout << "PRINT INVENTORY" << endl;
+
+    // Open the "feed.dat" file in binary read mode
+    ifstream inFile("feed.dat", ios::binary);
+    if (!inFile) {
+        cerr << "Error: Unable to open file." << endl;
+        return;
+    }
+
+    // Read all structures from the file and display in table form
+    Item currentItem;
+
+    Line(50);
+
+    cout << setw(20) << left << "Item Name"
+         << setw(10) << left << "Qty (bags)"
+         << setw(10) << left << "Price/bag"
+         << setw(10) << left << "Total" << endl;
     
+    Line(50);
+    float totalValue = 0;
+    while (inFile.read(reinterpret_cast<char*>(&currentItem), sizeof(currentItem))) {
+        cout << setw(20) << left << currentItem.item
+             << setw(10) << left << currentItem.qty
+             << setw(10) << left << currentItem.price
+             << setw(10) << left << (currentItem.qty * currentItem.price) << endl;
+        totalValue += currentItem.qty * currentItem.price;
+    }
+    inFile.close();
+
+    Line(50);
+    cout << setw(40) << right << "Total Value: " << setw(10) << left << totalValue << endl;
 }
 
 
+
+// Function to search for an item by name
 void searchItem() {
-    
+    //Not Needed
 }
 
 
+
+// Function to purchase an item by name and quantity
 void purchaseItem() {
-    
+    // Prompt user to input the name and quantity to purchase
+    char item[50];
+    int purchaseQty;
+    cout << "Enter the name of the item to purchase: ";
+    cin.getline(item, 50);
+    cout << "Enter the quantity to purchase: ";
+    cin >> purchaseQty;
+    cin.ignore(); // Ignore newline character
+
+    // Open the "feed.dat" file in binary mode
+    fstream file;
+    file.open("feed.dat", ios::in | ios::out | ios::binary);
+
+    // Check if the file is open
+    if (!file.is_open()) {
+        cout << "Error opening file." << endl;
+        return;
+    }
+
+    // Loop through all the structures in the file and find the record that matches the item name
+    Item buyItem;
+    bool found = false;
+    while (file.read(reinterpret_cast<char*>(&buyItem), sizeof(buyItem))) {
+        if (strcmp(buyItem.item, item) == 0) {
+            found = true;
+            break;
+        }
+    }
+
+    // Check if the item is found in the inventory
+    if (found) {
+        // Calculate the total price of the purchase
+        double purchasePrice = buyItem.price * purchaseQty;
+
+        // Check if the purchase quantity is greater than the available quantity
+        if (purchaseQty > buyItem.qty) {
+            cout << "Purchase quantity exceeds the available quantity." << endl;
+            file.close();
+            return;
+        }
+
+        // Update the quantity of the item in the record
+        buyItem.qty -= purchaseQty;
+
+        // Write the updated record back to the file
+        file.seekp(static_cast<int>(file.tellg()) - static_cast<int>(sizeof(buyItem)), ios::beg);
+        file.write(reinterpret_cast<char*>(&buyItem), sizeof(buyItem));
+
+        cout << "Purchase successful." << endl;
+        cout << "Total price: $" << purchasePrice << endl;
+    }
+    else {
+        cout << "Item not found." << endl;
+    }
+
+    // Close the file
+    file.close();
 }
+
 
 
 void calcTotal() {
-    
+    // Open the "feed.dat" file in binary mode for reading
+    ifstream inFile("feed.dat", ios::binary);
+    if (!inFile) {
+    cerr << "Error: Unable to open file for reading" << endl;
+    return;
+    }
+
+    // Initialize total value to 0
+    double totalValue = 0.0;
+
+    // Read each structure from the file and add the value to totalValue
+    Item currentItem;
+    while (inFile.read(reinterpret_cast<char*>(&currentItem), sizeof(currentItem))) {
+        totalValue += currentItem.qty * currentItem.price;
+    }
+
+    // Close the file
+    inFile.close();
+
+    // Display the total value
+    cout << "Total value of items on hand: $" << fixed << setprecision(2) << totalValue << endl;
+
 }
