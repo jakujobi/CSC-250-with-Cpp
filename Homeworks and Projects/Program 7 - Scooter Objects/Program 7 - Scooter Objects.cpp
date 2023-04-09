21e9b30bc44581fbc140f3e4b104fc8b6c62c978
akujobi365 <akujobi365@gmail.com>
2023-04-09 18:31:58 -0500

    Program 7 project setup

---
+++ Homeworks and Projects/Program 7 - Scooter Objects/Program 7 - Scooter Objects.cpp
@@ -0,0 +1,292 @@
+﻿// Program 7 - Scooter Objects.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+/* Program Requirements
+* Write a Scooter class that has the following **private** member variables:
+
+| Variable Name | Description |
+| --- | --- |
+| type | A string that holds the brand and model of the scooter. |
+| max_speed | A float that holds the maximum speed for the scooter. |
+| current_speed | A float that holds the current charge as a percentage. |
+
+In addition, the class should have the following constructors and member functions:
+
+- **Default Constructor:** The constructor should create an empty Scooter object, set the name to the empty string, and max and current speed to 0.
+- **2nd Constructor:** The second constructor should accept the scooter’s name and max speed as arguments. It should create an object with the requested name and max speed, setting the current speed to 0.
+- **Accessors:** Appropriate accessor functions to return the values stored in an object’s member variables.
+- **Mutators:** Appropriate mutator functions to set the values stored in an object’s member variables. Validate the data as is appropriate for the field
+- **`speedUp()`**: Increase speed by 2 mph, up to max_speed.
+- **`slowDown()`**: Decrease speed by 3 mph, down to 0.
+
+Demonstrate the class in a **program (main)** that creates a scooter object,
+    uses a loop to increase the speed in increments of 2 mph until reaching the max speed, printing the current speed after each increment.
+Then use a second loop to decrease the speed by decrements of 3 mph down to 0, printing speed after each increment.
+
+*/
+
+
+//Learnings
+/*
+	- learned to use \r to return to the beginning of the line
+		- https://www.codespeedy.com/use-of-r-in-cpp-carriage-return-to-escape-special-characters/
+
+*/
+
+//Struggles
+/*
+*/
+
+//Libraries
+#include <iostream> // cout, cin, endl
+#include <string>   // string class
+#include <iomanip>  // setw, setfill
+#include <fstream>  // ifstream, ofstream, fstream
+#include <cstring>  // strcpy
+#include <thread>   // this_thread::sleep_for in the loadingAnimation function
+#include <chrono>   // chrono::steady_clock
+using namespace std;
+
+
+//Constants
+const int ACCEL = 2; // Acceleration in mph
+const int DECEL = 3; // Deceleration in mph
+
+//Functions
+void loadingAnimation(); // displays a loading animation
+
+
+
+//Class Definition for Scooter
+class Scooter
+{
+	private:
+		string type;	// brand and model
+		float max_speed; // maximum speed in mph
+		float current_speed; // current speed in mph
+
+	public:
+		// Default Constructor
+		Scooter();
+		//Scooter() : type(""), max_speed(0.0), current_speed(0.0), time_driven(0.0) {}
+			//Learned this from stacckoverflow showing how to put a function on a single line
+
+		// 2nd Constructor that accepts the scooter’s name and max speed as arguments.
+		Scooter(string userTypeAndModel, float userMaxSpeed);
+
+		// Accessors
+		string getType(); //returns the type and model of scooter as a string
+		float getMaxSpeed(); // returns the max speed of the scooter as a float
+		float getCurrentSpeed(); // returns the current speed of the scooter as a float
+
+		// Mutators
+		void setType(string userTypeAndModel); //sets the type and model of the scooter
+		void setMaxSpeed(float userMaxSpeed); //sets the max speed of the scooter
+		void setCurrentSpeed(float userCurrentSpeed); //sets the current speed of the scooter
+		void speedUp(); // increases the speed of the scooter by the acceleration constant if the current speed is less than the max speed
+		void slowDown(); // decreases the speed of the scooter by the deceleration constant if the current speed is greater than 0
+		void displaySpeed(); // displays the current speed of the scooter like a digital speedometer
+		void hardStop(); // sets the current speed to 0
+		void honkHorn(); // honks a horn
+};
+
+// Member Methods
+
+// Default Constructor
+Scooter::Scooter() {
+	type = "";	// set to empty string using null
+	max_speed = 0;	// set to 0
+	current_speed = 0;	// set to 0
+}
+
+// 2nd Constructor that accepts the scooter’s name and max speed as arguments.
+Scooter::Scooter(string userTypeAndModel, float userMaxSpeed) {
+	type = userTypeAndModel;
+	max_speed = userMaxSpeed;
+	current_speed = 0;
+}
+
+// Accessors
+string Scooter::getType() {	//returns the type and model of scooter as a string
+	return type;
+}
+
+float Scooter::getMaxSpeed() { // returns the max speed of the scooter as a float
+	return max_speed;
+}
+
+float Scooter::getCurrentSpeed() { // returns the current speed of the scooter as a float
+	return current_speed;
+}
+
+// Mutators
+void Scooter::setType(string userTypeAndModel) {	//sets the type and model of the scooter
+	type = userTypeAndModel;
+}
+
+void Scooter::setMaxSpeed(float userMaxSpeed) { //sets the max speed of the scooter
+	max_speed = userMaxSpeed;
+}
+
+void Scooter::setCurrentSpeed(float userCurrentSpeed) { //sets the current speed of the scooter
+	current_speed = userCurrentSpeed;
+}
+
+void Scooter::speedUp() { // increases the speed of the scooter by the acceleration constant if the current speed is less than the max speed
+	if (current_speed < max_speed) {
+		current_speed += ACCEL;
+	}
+	else {
+		cout << "You are already at max speed!" << endl;
+	}
+}
+
+void Scooter::slowDown() { // decreases the speed of the scooter by the deceleration constant if the current speed is greater than 0
+	if (current_speed > 0) { // prevents the current speed from going below 0
+		current_speed -= DECEL;
+	}
+	else { // tells the user that they are already stopped if the current speed is 0
+		cout << "You are already stopped!" << endl;
+	}
+}
+
+void Scooter::displaySpeed() { // displays the current speed of the scooter like a digital speedometer
+	while (true) {
+		cout << "\rCurrent speed: " << current_speed << " km/h" << flush;
+		this_thread::sleep_for(chrono::milliseconds(500)); // 500 milliseconds pause
+	} // the while loop is used to keep the displaySpeed function running
+		// the \r is used to return to the beginning of the line
+		// the flush is used to flush the buffer
+		// Then it pauses for 500 milliseconds and uses the thread and chrono libraries
+}
+
+void Scooter::hardStop() {
+	current_speed = 0;
+}
+
+
+// Other Functions
+void Line(int num);	// draws a line of dots
+void loadingAnimation(); // displays a loading animation
+
+//!______________________________________________________________
+int main()
+{
+	// Variables
+	string userType; // brand and model
+	float userMaxSpeed; // maximum speed in mph
+
+	int choice; // user choice
+
+	// Loading Animation
+	loadingAnimation();
+
+do {
+		//Print the main menu
+		Line(50);
+		cout << "SCOOTER CONTROLLER" << endl;
+		do {
+			cout
+				<< "1. Speed Up\n"
+				<< "2. Slow Down\n"
+				<< "3. Hard Stop\n"
+				<< "4. Honk Horn\n"
+				<< "5. Exit\n"
+				<< endl;
+
+			//Get the user's choice
+			cout << "Enter your choice (1-8): ";
+			cin >> choice;
+			cin.ignore();
+			cin.clear();
+
+			//Validate the user's choice
+			if (choice < 1 || choice > 5) {
+				cout << "OOPS! Invalid choice. Please enter number from 1 to 5" << endl;
+			}
+		} while (choice < 1 || choice > 5); //repeat until the user enters a valid choice
+
+		switch (choice) { // Call the appropriate function based on user input
+		case 1:
+				scooter.speedUp();
+				break;
+		case 2:
+				scooter.slowDown();
+				break;
+		case 3:
+				scooter.hardStop();
+				break;
+		case 4:
+				scooter.honkHorn();
+				break;
+		case 5:
+				cout << "/nGoodBye" << endl;
+				loadingAnimation(3);
+				exit(0);
+		default:
+			cout << "OOPS! Invalid choice. Please enter number from 1 to 6" << endl;
+			break;
+		}
+	} while (choice != 5);
+
+	// User Input
+	cout << "Enter the type and model of the scooter: ";
+	getline(cin, userType);
+	cout << "Enter the maximum speed of the scooter: ";
+	cin >> userMaxSpeed;
+	// Object Creation
+	Scooter scooter(userType, userMaxSpeed);
+	// Display the current speed of the scooter
+	scooter.displaySpeed();
+	// Increase the speed of the scooter
+	scooter.speedUp();
+	// Decrease the speed of the scooter
+	scooter.slowDown();
+	// Pause the program
+	system("pause");
+	return 0;
+}
+
+//Functions
+// Asks the user for choice and calls the appropriate function
+
+// Prints a line of dashes
+void Line(int num) {
+	for (int i = 0; i < num; i++) {
+		cout << "-";
+	}
+	cout << endl;
+
+	//cout << setfill('-') << setw(50) << "-" << setfill(' ') << endl;
+}
+
+void createScooter (string *userType, float *userMaxSpeed){
+
+}
+
+
+void loadingAnimation(int seconds) {
+	string dots;    //to store the printed stuff
+	cout << "Loading...";
+
+	dots = "Beep! 🛴";
+
+	//cout << "Loading ";
+	//to make the loading animation
+	for (int i = 0; i < seconds; i++) {
+		this_thread::sleep_for(chrono::seconds(1));
+		cout << dots << flush << "\a";
+	}
+	cout << endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
